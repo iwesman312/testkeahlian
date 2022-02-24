d8bffba044aab10e32a1b1fb8a8e6b72fbff101f
iwesman312 <100330162+iwesman312@users.noreply.github.com>
2022-02-24 14:55:06 +0700

    Add files via upload

---
+++ bilangangenapdash.php
@@ -0,0 +1,16 @@
+<?php
+function pemisah($thestring)
+{
+    $a = strlen($thestring);
+    $result = '';
+    for ($i = 0; $i <= $a; $i++) {
+        $char = substr($thestring, $i, 1);
+        if ($char % 2 == 0 && $i != $a) {
+            $result = $result . $char . "-";
+        } else {
+            $result = $result . $char;
+        }
+    }
+    return $result;
+}
+echo pemisah(43356);
